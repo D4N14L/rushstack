ecf04fd0701bb1efcb3e6dad714acc80639ac98a
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2025-04-24 17:17:13 -0700

    Add an eslint 9 test project

---
+++ build-tests/eslint-9-test/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    '@rushstack/eslint-config/profile/node-trusted-tool',
+    '@rushstack/eslint-config/mixins/friendly-locals'
+  ],
+  parserOptions: { tsconfigRootDir: __dirname },
+
+  overrides: [
+    /**
+     * Override the parser from local-eslint-config. Since the config is coming
+     * from the workspace instead of the external NPM package, the versions of ESLint
+     * and TypeScript that the config consumes will be resolved from the devDependencies
+     * of the config instead of from the eslint-7-test package. Overriding the parser
+     * ensures that the these dependencies come from the eslint-7-test package. See:
+     * https://github.com/microsoft/rushstack/issues/3021
+     */
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser'
+    }
+  ]
+};
